7d76eb5dc6c0f518e8785b377772e54bd4a607df
alexeikeuda <alexei.ramo@edu.keuda.fi>
2022-03-22 09:46:41 +0200

    ...

--- demo8/kirjautuminen.js
+++ demo8/kirjautuminen.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 
-function onkoKirjautunut(){
-    if(localStorage.getItem("kirjautunut") === "kylla") {
+function onkoKirjautunut() {
+    if (localStorage.getItem("kirjautunut") === "kylla") {
         document.getElementById("tervetulo_teksti").textContent += localStorage.getItem("nimi");
-        document.getElementById("kirjautumis_lomake").style.display = "none";
+        document.getElementById("kirjautumis_lomake")
+        .style.display = "none";
     }
 }
 
